afead54cc0d0ef021633be66f50ccd991813d632
Ra-Ha-Til <rahatil880@gmail.com>
2025-08-05 20:36:04 +0600

    routes added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PortfolioController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PortfolioController::class, 'home'])->name('home');
+Route::get('/education', [PortfolioController::class, 'education'])->name('education');
+Route::get('/specialization', [PortfolioController::class, 'specialization'])->name('specialization');
+Route::get('/skills', [PortfolioController::class, 'skills'])->name('skills');
+Route::get('/blog', [PortfolioController::class, 'blog'])->name('blog');
+Route::get('/projects', [PortfolioController::class, 'projects'])->name('projects');
+Route::get('/about', [PortfolioController::class, 'about'])->name('about');
